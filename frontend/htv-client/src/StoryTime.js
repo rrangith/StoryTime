e5b69055cdba93c1444518ca4e6354eb1b211dfe
Rahul Rangith <rrangith@edu.uwaterloo.ca>
2019-02-23 09:12:20 -0500

    Mic (#7)
    * adding react speech recognition
    
    * making client send getImage request when user takes a pause

--- frontend/htv-client/src/StoryTime.js
+++ frontend/htv-client/src/StoryTime.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom';
 import Webcam from 'react-webcam';
 import axios from 'axios';
 import styled from 'styled-components';
+import SpeechRecognition from 'react-speech-recognition'
 
 const NavBar = styled.div`
     height: 8vh;
     background-color: #234569;
     position: relative;
-   
+
 `;
 const TitleDiv = styled.div`
     display: inline-block;
@@ -20,56 +22,88 @@ const TitleText = styled.div`
     color: #fff;
     display: inline-block;
     padding-left:15%;
-    
+
 `;
 
 const Invisible = styled.div`
   opacity:1;
 `;
 
+const propTypes = {
+  // Props injected by SpeechRecognition
+  transcript: PropTypes.string,
+  resetTranscript: PropTypes.func,
+  browserSupportsSpeechRecognition: PropTypes.bool
+};
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: ''
-        }
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          text: '',
+          canSendRequest: true
+      };
+  }
+
+  setRef = webcam => {
+      this.webcam = webcam;
+  };
+
+  capture = () => {
+      const imageSrc = this.webcam.getScreenshot();
+      axios.get('http://localhost:5000/getImage', {
+          params: {image: imageSrc, text: this.state.text}
+      }).then((response) => {
+          console.log(response);
+      });
+      this.props.resetTranscript();
+      this.setState({text: ''});
+  }
+
+  compareLastTranscript = () => {
+    if (this.state.text === this.props.transcript) {
+      if (this.state.canSendRequest) {
+        this.capture();
+        this.setState({canSendRequest: false})
+      }
+    } else {
+      this.setState({text: this.props.transcript, canSendRequest: true});
     }
+  }
 
-    setRef = webcam => {
-        this.webcam = webcam;
-    };
+  componentDidMount() {
+    setInterval(this.compareLastTranscript, 750);
+  }
 
-    capture = () => {
-        const imageSrc = this.webcam.getScreenshot();
-        axios.get('http://localhost:5000/getImage', {
-            params: {image: imageSrc, text: this.state.text}
-        }).then((response) => {
-            console.log(response);
-        });
+  render() {
+    const { transcript, resetTranscript, browserSupportsSpeechRecognition } = this.props;
+    if (!browserSupportsSpeechRecognition) {
+      return null;
     }
-
-    render() {
-        return (
-            <div>
-                <NavBar>
-                    <Link to=''>
-                        <TitleDiv>
-                            <TitleText>
-                                storify
-                            </TitleText>
-                        </TitleDiv>
-                    </Link>
-                </NavBar>
-                <Invisible>
-                    <Webcam
-                        height={350}
-                        width={400}
-                        screenshotFormat="image/jpeg"
-                        ref={this.setRef}
-                    />
-                </Invisible>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <NavBar>
+                <Link to=''>
+                    <TitleDiv>
+                        <TitleText>
+                            {transcript}
+                        </TitleText>
+                    </TitleDiv>
+                </Link>
+            </NavBar>
+            <Invisible>
+                <Webcam
+                    height={350}
+                    width={400}
+                    screenshotFormat="image/jpeg"
+                    ref={this.setRef}
+                />
+            </Invisible>
+        </div>
+    );
+  }
 }
+
+App.propTypes = propTypes;
+
+export default SpeechRecognition(App)
