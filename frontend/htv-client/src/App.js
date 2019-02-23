cff2099659c03b40f9a543d9f3e24bcd6e6c2c05
Wayne Ferrao <wayne.ferrao93@gmail.com>
2019-02-22 23:24:16 -0500

    Initialized FrontEnd

---
+++ frontend/htv-client/src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import {BrowserRouter as Router, Route,} from 'react-router-dom';
+import StoryTime from './StoryTime';
+
+const HomeContainer = styled.div`
+  background-color: #123456;
+  height: 100%;
+  width: 100%;
+  display: block;
+`;
+
+
+export default class App extends Component {
+    render() {
+        const Home = () => (
+            <div>
+                <h1>
+                    KAKASHI
+                </h1>
+            </div>
+        );
+        return (
+            <Router>
+                <div>
+                    <Route exact path='' component={Home}/>
+                    <Route exact path='/StoryTime' component={StoryTime}/>
+                </div>
+            </Router>
+        );
+    }
+}
+
+
